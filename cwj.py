8cd0f51d200b44e191466e48b7bfadf2aa5e852a
zqq <227811181@qq.com>
2025-11-16 16:19:17 +0800

    test

---
+++ cwj.py
@@ -0,0 +1,12 @@
+print("Hello World from Anaconda!")
+
+# 测试numpy库是否可用
+import numpy as np
+print("NumPy version:", np.__version__)
+
+# 测试matplotlib库是否可用
+try:
+    import matplotlib
+    print("Matplotlib version:", matplotlib.__version__)
+except ImportError:
+    print("Matplotlib not installed")
